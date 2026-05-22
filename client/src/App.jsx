cf7689b87454e77468405e7cb761d0b64f122e6c
ashutosh <your@abc.com>
2026-05-22 19:27:41 +0530

    fixed the problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     fetchTasks();
   };
 
-  let ashu = 'unused variable here';
+ // let ashu = 'unused variable here';
 
   return (
     <div className="min-h-screen bg-gray-50">
